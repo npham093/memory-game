403ce44470c8151b67239a4f23991cb3f83cdb9d
npham093 <52163211+npham093@users.noreply.github.com>
2019-07-20 14:48:55 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,58 +1,66 @@
 let count = 0;
 let moveCounter = 0;
+let gameCleared = false;
+let rate = '★★★';
 
 var cardOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 var cardShownList = [];
 var matchedCards = [];
 
-
 function toggleCard(el) {
 	if (document.getElementById('startTimer').style.display === 'none') {
 		if (el.classList.contains('hidden')) {
-			el.classList.add('show');
-			el.classList.remove('hidden');
-			addCardsToList(el);
-			moveCounter = moveCounter + .5;
-			
+			if (cardShownList.length < 2) {
+				el.classList.add('show');
+				el.classList.remove('hidden');
+				addCardsToList(el);
+				moveCounter = moveCounter + .5;
+			}	
 		} else {
 			el.classList.add('hidden');
 			el.classList.remove('show');
+			cardShownList = [];
 		}
 	}
 }
 
 function myTimer() {
 	document.getElementById('startTimer').style.display = 'none';
-  	document.getElementById("timerCounter").innerHTML = "Timer: " + count + " seconds";
-  	count += 1;
-
+	document.getElementById("timerCounter").innerHTML = "<b>Timer: </b>" + count + " seconds";
+	document.getElementById("star-rating").innerHTML = rate;
 	if (moveCounter % 1 === 0) {
-		document.getElementById('move-counter').innerHTML = 'Moves: ' + moveCounter;
+		document.getElementById('move-counter').innerHTML = '<b>Moves: </b>' + moveCounter;
+	}
+	if (!gameCleared) {
+		count += 1;
 	}
 }
 
 function resetGame(myVar) {
-	hideAllCards();
 	clearInterval(myVar);
+	hideAllCards();
 	changeCardOrders();
 	starRating();
 	count = 0;
 	moveCounter = 0;
+	gameCleared = false;
+	rate = '★★★';
+	matchedCards = [];
 }
 
 // Used the shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 function changeCardOrders() {
@@ -70,6 +78,9 @@ function hideAllCards() {
 
 	while (shownClassList.length !== 0) {
 		for (var i = 0; i < shownClassList.length; i++) {
+			shownClassList[i].style.backgroundColor = '#eaeaea';
+			shownClassList[i].style.color = '#488e8a';
+			shownClassList[i].style.boxShadow = 'none';
 			shownClassList[i].classList.add('hidden');
 			shownClassList[i].classList.remove('show');
 			console.log(shownClassList);
@@ -79,10 +90,21 @@ function hideAllCards() {
 
 
 function addCardsToList(el) {
+	console.log(cardShownList)
 	if (cardShownList.length < 2) {
 		cardShownList.push([el.className.split(' ')]);
 	}
 	checkMatch(el);
+}
+
+function checkGameEnd() {
+	if (matchedCards.length == 8) {
+		gameCleared = true;
+
+		document.getElementById("totalRate").innerHTML = rate;
+		document.getElementById("counterTotal").innerHTML = "It took "+ count + " seconds to clear the game!";
+		document.getElementById("myModal").style.display = "block";
+	}
 }
 
 function checkMatch(el) {
@@ -90,83 +112,76 @@ function checkMatch(el) {
 		if (cardShownList[0][0][0] === cardShownList[1][0][0]) {
 			matchedCards.push(cardShownList[0][0][0]);
 			for (var i = 0; i < matchedCards.length; i++) {
-				document.getElementsByClassName(matchedCards[i])[0].style.backgroundColor = 'blue';
-				document.getElementsByClassName(matchedCards[i])[1].style.backgroundColor = 'blue';
+				document.getElementsByClassName(matchedCards[i])[0].style.backgroundColor = 'blueviolet';
+				document.getElementsByClassName(matchedCards[i])[1].style.backgroundColor = 'blueviolet';
 				document.getElementsByClassName(matchedCards[i])[0].style.color = 'white';
 				document.getElementsByClassName(matchedCards[i])[1].style.color = 'white';
+				document.getElementsByClassName(matchedCards[i])[0].style.boxShadow = '0px 0px 8px 3px white';
+				document.getElementsByClassName(matchedCards[i])[1].style.boxShadow = '0px 0px 8px 3px white';
 			}
 			cardShownList = [];
 			console.log(matchedCards)
 		}
-		if (cardShownList[0][0][0] !== cardShownList[1][0][0]) {
+		else {
 			console.log('failed');
 			console.log(cardShownList[0][0][0])
 			setTimeout(hideMissedCards,700);
 		}
 	}
 	if (cardShownList.length > 2) {
+		cardShownList = [];
 		return;
 	}
+
+	checkGameEnd();
+	starRating();
 }
 
 function hideMissedCards() {
+	if (cardShownList.length > 0) {
+		var unmatchedCard1 = document.getElementsByClassName(cardShownList[0][0][0]);
+		var unmatchedCard2 = document.getElementsByClassName(cardShownList[1][0][0]);
 
-	var unmatchedCard1 = document.getElementsByClassName(cardShownList[0][0][0]);
-	var unmatchedCard2 = document.getElementsByClassName(cardShownList[1][0][0]);
-
-	for (var i = 0; i < unmatchedCard1.length; i++) {
-		unmatchedCard1[i].style.fontSize = '10em';
-		unmatchedCard1[i].classList.add('hidden');
-		unmatchedCard1[i].classList.remove('show');
-		unmatchedCard1[i].style.fontSize = '8em';
-	}
-	for (var i = 0; i < unmatchedCard2.length; i++) {
-		unmatchedCard1[i].style.fontSize = '10em';
-		unmatchedCard2[i].classList.add('hidden');
-		unmatchedCard2[i].classList.remove('show');
-		unmatchedCard1[i].style.fontSize = '8em';
+		for (var i = 0; i < unmatchedCard1.length; i++) {
+			unmatchedCard1[i].classList.add('hidden');
+			unmatchedCard1[i].classList.remove('show');
+		}
+		for (var i = 0; i < unmatchedCard2.length; i++) {
+			unmatchedCard2[i].classList.add('hidden');
+			unmatchedCard2[i].classList.remove('show');
+		}
 	}
 	cardShownList = [];
 }
 
 function starRating() {
-	if (moveCounter >= 20) {
-		document.getElementById('star-rating').innerText = '★';
+	if (moveCounter >= 40) {
+		rate = '★';
 	}
-	else if(moveCounter >= 3 ) {
-	document.getElementById('star-rating').innerText = '★★';
+	else if(moveCounter >= 30 ) {
+		rate= '★★';
 	}
-
-	else if(moveCounter >= 0) {
-		document.getElementById('star-rating').innerText = '★★★';
+	else if(moveCounter >= 20) {
+		rate = '★★★';
 	}
+	document.getElementById('star-rating').innerText = rate;
 }
 
-starRating();
 
 // modal code from https://www.w3schools.com/howto/howto_css_modals.asp
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+	modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+function playAgain(myVar) {
+	resetGame(myVar);
+	modal.style.display = "none";
 }
